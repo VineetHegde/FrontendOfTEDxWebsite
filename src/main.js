5c28b5477175b0172fc7e53df1a6b3421280a72d
VineetHegde <168419325+VineetHegde@users.noreply.github.com>
2025-08-30 19:57:37 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -66,6 +66,7 @@
 // }
 
 
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -108,73 +109,6 @@ function App() {
               <div id="sponsors">
                 <Sponsors />
               </div>
-            </>
-          }
-        />
-        <Route path="/get-ticket" element={<GetTicket />} />
-        <Route path="/success" element={<SuccessPage />} />
-      </Routes>
-      <Footer />   {/* Always visible */}
-    </Router>
-  );
-}
-
-// Check if root element exists before rendering
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element not found. Make sure your index.html has a div with id='root'");
-}
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Element } from "react-scroll";
-
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Speakers from "./components/Speakers";
-import Sponsors from "./components/Sponsors";
-import Footer from "./components/Footer";
-import GetTicket from "./components/GetTicket";
-import SuccessPage from "./components/SuccessPage";
-
-import "../src/index.css";
-
-function App() {
-  return (
-    <Router>
-      <Header />   {/* Always visible */}
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              {/* Wrap sections in <Element> with matching names */}
-              <Element name="home">
-                <Home />
-              </Element>
-
-              {/* Uncomment and add Theme component when ready
-              <Element name="theme">
-                <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-                  <h1 className="text-4xl">Theme Section</h1>
-                </div>
-              </Element>
-              */}
-
-              <Element name="speakers">
-                <Speakers />
-              </Element>
-
-              <Element name="sponsors">
-                <Sponsors />
-              </Element>
             </>
           }
         />
